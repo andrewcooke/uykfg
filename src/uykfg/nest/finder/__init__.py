6dcc50eb2b98d4565e5fa00afe118b4533e1b60a
andrew cooke <andrew@acooke.org>
2013-02-06 09:36:59 -0300

    relax album criteria now last used

--- src/uykfg/nest/finder/__init__.py
+++ src/uykfg/nest/finder/__init__.py
@@ -67,7 +67,7 @@ class Finder:
             except URLError as e: debug(e)
         if artists:
             (id, name), score = artists.most_common(1)[0]
-            if score > max(3, len(titles) / 2) and len(artists) < 10:
+            if score > max(3, len(titles) / 2):
                 debug('voted for %s:%s (%d)' % (name, id, score))
                 return self._artist(session, artist, id, name)
             else:
