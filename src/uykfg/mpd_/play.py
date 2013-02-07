4697dad7717b659eb2aa5aef317f43cd73c425b8
andrew cooke <andrew@acooke.org>
2013-02-07 20:34:13 -0300

    read status

--- src/uykfg/mpd_/play.py
+++ src/uykfg/mpd_/play.py
@@ -1,4 +1,4 @@
-
+from logging import debug
 from time import sleep
 
 from mpd import MPDClient
@@ -8,12 +8,24 @@ def play_links(session):
     mpd = MPDClient()
     mpd.connect("localhost", 6600)
     while True:
-        if almost_empty(mpd): queue_next(mpd, session)
+        if empty(mpd):
+            queue_random(mpd, session)
+        else:
+            penultimate = almost_empty(mpd)
+            if penultimate: queue_next(mpd, session, penultimate)
         sleep(10)
 
-def almost_empty(mpd):
-    print(mpd.playlistinfo(1))
-    return False
+def empty(mpd):
+    return bool(mpd.playlistinfo(0))
 
-def queue_next(mpd, session):
-    pass
+def almost_empty(mpd):
+    penultimate = mpd.playlistinfo(1)
+    if penultimate: penultimate = penultimate[0]
+    if not mpd.playlistinfo(1): return penultimate
+    else: return False
+
+def queue_next(mpd, session, penultimate):
+    debug('next')
+
+def queue_random(mpd, session):
+    debug('random')
