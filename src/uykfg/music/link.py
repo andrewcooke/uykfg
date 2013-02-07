740483ef95df6e6630ca8add1c1dac7693bf5556
andrew cooke <andrew@acooke.org>
2013-02-07 18:33:20 -0300

    allow self link

--- src/uykfg/music/link.py
+++ src/uykfg/music/link.py
@@ -22,8 +22,7 @@ def link_same_album(session, src):
     artist1, track1, track2, artist2 = map(aliased, [Artist, Track, Track, Artist])
     commit = False
     for dst in session.query(artist1).join(track1, Album, track2, artist2)\
-            .filter(artist2.id == src.id, artist1.id != artist2.id)\
-            .group_by(artist1.id).all():
+    .filter(artist2.id == src.id).group_by(artist1.id).all():
         debug('linking %s to %s via common album' % (src.name, dst.name))
         session.add(Link(src=src, dst=dst))
         commit = True
