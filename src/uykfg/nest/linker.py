134869cff46bd3c65b0bdebefee19fc2007f2979
andrew cooke <andrew@acooke.org>
2013-02-07 16:58:30 -0300

    incremental

--- src/uykfg/nest/linker.py
+++ src/uykfg/nest/linker.py
@@ -16,25 +16,35 @@ class Linker:
     def __init__(self, config, session):
         self._api = Cache(RateLimitingApi(config.api_key), session)
 
-    def link(self, session, src):
+    def link(self, session, src, target=8):
         try:
-            commit = False
-            id = session.query(NestArtist)\
-                    .join('artists')\
-                    .filter(Artist.id == src.id).one().id
-            for artist in unpack(self._api('artist', 'similar', id=id), 'response', 'artists'):
-                try:
-                    nest_artist = session.query(NestArtist)\
-                            .filter(NestArtist.id == artist['id']).one()
-                    for dst in nest_artist.artists:
-                        if not session.query(Link)\
-                                .filter(Link.src == src, Link.dst == dst)\
-                                .count():
-                            debug('linking %s to %s' % (src.name, dst.name))
-                            session.add(Link(src=src, dst=dst))
-                            commit = True
-                except NoResultFound:
-                    pass
-            if commit: session.commit()
+            id = session.query(NestArtist).join('artists').filter(Artist.id == src.id).one().id
+            start, results, found = 0, 12, 0
+            while found < target and results < 100:
+                found += self._link_delta(session, src, start, results, target, id)
+                start = results
+                results *= 2
         except NoResultFound:
             debug('no nest artist for %s' % src.name)
+        except (AttributeError, IndexError) as e:
+            debug(e)
+
+    def _link_delta(self, session, src, start, results, target, id):
+        found = 0
+        for artist in unpack(self._api('artist', 'similar', id=id,
+                                       start=start, results=results-start),
+                             'response', 'artists'):
+            try:
+                nest_artist = session.query(NestArtist)\
+                        .filter(NestArtist.id == artist['id']).one()
+                for dst in nest_artist.artists:
+                    if found < target and\
+                            not session.query(Link).filter(Link.src == src, Link.dst == dst).count():
+                        debug('linking %s to %s' % (src.name, dst.name))
+                        session.add(Link(src=src, dst=dst))
+                        found += 1
+            except NoResultFound:
+                pass
+        if found: session.commit()
+        return found
+
