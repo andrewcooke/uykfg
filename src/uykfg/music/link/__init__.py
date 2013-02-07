b97656d2d50588f4eac48084732a4bfde2f10557
andrew cooke <andrew@acooke.org>
2013-02-07 14:04:41 -0300

    clean

--- src/uykfg/music/link/__init__.py
+++ src/uykfg/music/link/__init__.py
@@ -15,12 +15,17 @@ def link_all(session, linker):
     debug('done!')
 
 def delete_src(session, artist):
+    debug('deleting links from %s' % artist.name)
     session.query(Link).filter(Link.src_id == artist.id).delete()
     session.commit()
 
 def link_same_album(session, src):
     artist1, track1, track2, artist2 = map(aliased, [Artist, Track, Track, Artist])
+    commit = False
     for dst in session.query(artist1).join(track1, Album, track2, artist2)\
             .filter(artist2.id == src.id, artist1.id != artist2.id)\
             .group_by(artist1.id).all():
-        print(src, dst)
+        debug('linking %s to %s via common album' % (src.name, dst.name))
+        session.add(Link(src=src, dst=dst))
+        commit = True
+    if commit: session.commit()
