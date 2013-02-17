7c8731f26f9d3438bd551d2bb8f3f67e3d140d02
andrew cooke <andrew@acooke.org>
2013-02-17 20:56:27 -0300

    drop artists

--- src/uykfg/drop.py
+++ src/uykfg/drop.py
@@ -7,11 +7,11 @@ from uykfg.music.db.catalogue import Artist, Track, Album
 from uykfg.support.configure import Config
 
 
-def drop(artist):
-    warning('deleting %s' % artist)
+def drop(name):
+    warning('deleting %s' % name)
     config = Config.default()
     session = startup(config)
-    for artist in session.query(Artist).filter(Artist.name == artist).all():
+    for artist in session.query(Artist).filter(Artist.name == name).all():
         for track in session.query(Track).filter(Track.artist_id == artist.id).all():
             info('deleting %s from %s' % (track.name, track.album))
             session.delete(track)
