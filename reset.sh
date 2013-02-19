e2f8562478bfebafe20d2741bd863def282409b8
andrew cooke <andrew@acooke.org>
2013-02-19 09:48:03 -0300

    reset

--- reset.sh
+++ reset.sh
@@ -7,5 +7,7 @@ drop table nest_artists;
 drop table music_artists;
 drop table music_albums;
 drop table music_tracks;
+drop table music_artists_and_music_tags;
+drop table music_tags;
 .tables
 EOF
