571e1291db8be96e4aac8e1bc38a05d66e6b91fc
andrew cooke <andrew@acooke.org>
2013-02-19 17:52:17 -0300

    show command

--- src/uykfg/show.py
+++ src/uykfg/show.py
@@ -1,8 +1,10 @@
 
 from sys import argv
+from sqlalchemy.orm.exc import NoResultFound
 
 from uykfg.music.db import startup
 from uykfg.music.db.catalogue import Artist
+from uykfg.nest.db import NestArtist
 from uykfg.support.configure import Config
 
 
@@ -11,7 +13,9 @@ def show(names):
     session = startup(config)
     for name in names:
         for artist in session.query(Artist).filter(Artist.name == name).all():
-            print(artist.name)
+            try: nest_artist = session.query(NestArtist).filter(NestArtist.artists.any(artist)).one()
+            except NoResultFound: nest_artist = 'no nest artist'
+            print('%s (%s)' % (artist.name, nest_artist.name))
             print(' tags:\n  %s' % ' '.join('"%s"' % tag.text for tag in artist.tags))
 
 
