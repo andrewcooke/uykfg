75d788f6348b71d9185d2681562251ac7112dbc7
andrew cooke <andrew@acooke.org>
2013-02-19 09:26:39 -0300

    diags for drop

--- drop.sh
+++ drop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+PARAMS=""
+for PARAM in "$@"
+do
+  PARAMS="${PARAMS} \"${PARAM}\""
+done
 source env/bin/activate
-PYTHONPATH=src python src/uykfg/drop.py $@
+PYTHONPATH=src python src/uykfg/drop.py $PARAMS
+
