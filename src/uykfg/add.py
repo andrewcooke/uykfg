fa0879feea00bbafa03ae42b57c5260edce7801c
andrew cooke <andrew@acooke.org>
2013-02-19 17:12:10 -0300

    add command

--- src/uykfg/add.py
+++ src/uykfg/add.py
@@ -1,14 +1,13 @@
 
 from logging import debug
 from sys import argv
-from sqlalchemy import alias
+from sqlalchemy.orm import aliased
 
 from sqlalchemy.sql.functions import random
 
 from uykfg.mpd_.add import add_tracks
 from uykfg.music.db import startup
 from uykfg.music.db.catalogue import Track, Artist
-from uykfg.music.db.tags import Tag
 from uykfg.support.configure import Config
 
 
@@ -17,7 +16,8 @@ def add(count, names):
     session = startup(config)
     tracks = session.query(Track)
     for name in names:
-        artist = alias(Artist)
+        debug('filtering by %s' % name)
+        artist = aliased(Artist)
         tracks = tracks.join(artist).join(artist.tags).filter(artist.tags.any(name))
     tracks = tracks.order_by(random()).limit(count)
     add_tracks(session, config, tracks.all())
